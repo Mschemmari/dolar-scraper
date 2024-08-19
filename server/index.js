82cec692d5b3849f5e8d51225d4a35d148e01afb
Mariano Schemmari <mariano@truckatlas.com>
2024-08-19 11:36:41 -0300

    dolar-scraper

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const http = require('http');  // Required for integrating with Socket.IO
 const axios = require('axios');
 const cheerio = require('cheerio');
 const { Server } = require('socket.io');
+const e = require('express');
+const { log } = require('console');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,43 +16,72 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Variable to store the scraped data
-let scrapedData = {};
+let scrapedDataContainer = (newData)=>{
+  let dataArray = [];
+  let dataSet = new Set();
+  dataArray.push(newData);  // Add to the array
+  dataSet.add(newData);  // Track in the Set
+  return dataSet
+}
 
 // Function to scrape data
 const scrapeData = async () => {
+  
   try {
-    // Fetch the HTML of the page
-    const { data } = await axios.get('https://dolarhoy.com/');
-    
-    // Load the HTML into cheerio
-    const $ = cheerio.load(data);
-
-    // Define the selectors for the data you want to scrape
-    const buyPrice = $('div[data-sell]').attr('data-sell');
-    const sellPrice = $('div[data-buy]').attr('data-buy');
-    const officialBuy = $('div[data-official-buy]').attr('data-official-buy');
-    const officialSell = $('div[data-official-sell]').attr('data-official-sell');
-
-    // Update the scraped data
-    scrapedData = {
-      buyPrice,
-      sellPrice,
-      officialBuy,
-      officialSell,
-      lastUpdated: new Date().toISOString()
-    };
-
-    console.log('Data scraped and updated:', scrapedData);
-
+    const currencyCode = {
+      ARSB: 'Dólar Blue',
+      ARSCONT: 'Dólar Contado con Liquidación',
+      ARSTAR: 'Dólar Tarjeta',
+      ARSMEP: 'Dólar MEP'
+    }
+    const currencies = Array.from(Object.keys(currencyCode))
+    const baseUrl = 'https://www.cronista.com/MercadosOnline/dolar.html'
+    const response = await axios.get(baseUrl)
+        
+        
+    const $ = cheerio.load(response.data);
+    const row1 = $('#market-scrll-2').find('tr').toArray();
+    const row2 = $('#market-scrll-3').find('tr').toArray();
+      const data1 = row1.map((val) => {
+      const name = $(val).find('.name a').text()
+      const precioCompra = $(val).find('.buy-wrapper .buy-value').text().replace(/\$/g, '');
+      const precioVenta = $(val).find('.sell-wrapper .sell-value').text().replace(/\$/g, '');
+      // const precioAnterior = $(val).find('.previousvalue .val').text().replace(/\$/g, '');
+      const variacion = $(val).find('.percentage').text().replace(/\%/g, '');
+      return values = {
+        name,
+        precioCompra,
+        precioVenta,
+        // precioAnterior,
+        variacion,
+        actualizado: new Date().toISOString()
+      };
+    })
+    const data2 = row2.map((val) => {
+      const name = $(val).find('.name a').text()//.replace(/\$/g, '');
+      const precioCompra = $(val).find('.buy-wrapper .buy-value').text().replace(/\$/g, '');
+      const precioVenta = $(val).find('.sell-wrapper .sell-value').text().replace(/\$/g, '');
+      // const precioAnterior = $(val).find('.previousvalue .val').text().replace(/\$/g, '');
+      const variacion = $(val).find('.percentage').text().replace(/\%/g, '');
+      return values = {
+        name,
+        precioCompra,
+        precioVenta,
+        // precioAnterior,
+        variacion,
+        actualizado: new Date().toISOString()
+      };
+    })
+    console.log(scrapedDataContainer([...data1, ...data2]));
     // Emit the updated data to all connected clients via WebSocket
-    io.emit('data-update', scrapedData);
-  } catch (error) {
+    io.emit('data-update', scrapedDataContainer([...data1, ...data2]));
+  }  catch (error) {
     console.error('Error scraping data:', error);
   }
 };
 
 // Run the scraping function every 1 second
-setInterval(scrapeData, 1000);
+setInterval(scrapeData, 20000);
 
 // Initial scrape
 scrapeData();
@@ -75,5 +106,5 @@ io.on('connection', (socket) => {
 
 // Start the server
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  // console.log(`Server is running on port ${PORT}`);
 });
