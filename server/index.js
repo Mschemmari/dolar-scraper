d14a4a793e1e6658d4efafdc039ea16383d0e266
mschemmari <mschemmari@mobilecomputing.com.ar>
2025-05-07 12:29:46 -0300

    add websocket conectivity

--- server/index.js
+++ server/index.js
@@ -1,42 +1,36 @@
 const express = require('express');
 const http = require('http');  // Required for integrating with Socket.IO
 const { Server } = require('socket.io');
-const e = require('express');
-const {scrapeData} = require('./utils');
+const {fetchRates} = require('./utils');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Create an HTTP server
 const server = http.createServer(app);
-
-// Initialize Socket.IO
 const io = new Server(server);
-setInterval(scrapeData, 9000);
-// Initial scrape
-scrapeData();
-// Endpoint to serve the latest scraped data via HTTP
-app.get('/scrape', (req, res) => {
-  console.log(req);
-  console.log('aaaa');
-  res.json(formatData(...data1, ...data2));
-  
-});
 
-// Handle WebSocket connections
+const emitUpdates = (rates, modified) => {
+   if(modified.length > 0){
+    io.emit('data-update', rates, modified);
+    console.log('🔁 Emitted updated rates to all clients');
+   }
+ };
+
 io.on('connection', (socket) => {
-  console.log('A client connected:', socket.id);
-  
-  // Send the current data to the new client
-  socket.emit('data-update', );
+  console.log('🟢 Client connected:', socket.id);
+  const getRates = setInterval(async () => {
+    const { rates, modified } = await fetchRates();
+    emitUpdates(rates,  modified );
+  }, 9000);
 
-  // Handle disconnection
   socket.on('disconnect', () => {
-    console.log('A client disconnected:', socket.id);
+    console.log('🔴 Client disconnected:', socket.id);
+    clearInterval(getRates);
   });
 });
 
-// Start the server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
